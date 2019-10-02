1e3823c88922d3fdddf22fe9402aabbe3c98b0e6
Kenny Kerr <kekerr@microsoft.com>
2019-10-01 20:12:04 -0700

    broke-enums

--- src/enums.rs
+++ src/enums.rs
@@ -1,19 +1,27 @@
 use crate::tables::*;
+use crate::database::*;
 
 trait CodedIndex
 {
-    fn bits(&self) -> u32;
+    // fn bits(&self) -> u32;
+    fn decode<'a>(db: &'a Database, index: u32) -> TypeDefOrRef<'a>;
 }
 
 enum TypeDefOrRef<'a>
 {
     TypeDef(TypeDefRow<'a>),
-    TypeRef(TypeRefRow<'a>),
+    //TypeRef(TypeRefRow<'a>),
     // TypeSpec,
 }
 
-impl<'a> CodedIndex for TypeDefOrRef<'a>
+impl<'a> CodedIndex<'a> for TypeDefOrRef<'a>
 {
-    fn bits(&self) -> u32 { 2 }
-    // TODO: add decode function that takes a u32.
+    // fn bits(&self) -> u32 { 2 }
+    fn decode(db: &'a Database, index: u32) -> TypeDefOrRef<'a>
+    {
+        match index & ((1 << 2) - 1)
+        {
+            0 => TypeDefOrRef::TypeDef(TypeDefRow { db: db, index: (index >> 2) - 1 }),
+        }
+    }
 }
