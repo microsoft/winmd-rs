2868258cd40d57af61c7f29106e23462d01d89da
Kenny Kerr <kenny.kerr@microsoft.com>
2019-11-01 16:21:24 -0700

    sep

--- src/flags.rs
+++ src/flags.rs
@@ -10,9 +10,9 @@ pub struct TypeAttributes(pub(crate) u32);
 
 impl TypeAttributes {
     pub fn windows_runtime(&self) -> bool {
-        self.0 & 0b100000000000000 != 0
+        self.0 & 0b100_0000_0000_0000 != 0
     }
     pub fn interface(&self) -> bool {
-        self.0 & 0b100000 != 0
+        self.0 & 0b10_0000 != 0
     }
 }
