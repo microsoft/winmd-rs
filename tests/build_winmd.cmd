032939a4d4cd560638dc6fb77988b3c6c30d547b
Kenny Kerr <kenny.kerr@microsoft.com>
2019-11-19 15:17:42 -0800

    build

--- tests/build_winmd.cmd
+++ tests/build_winmd.cmd
@@ -1,2 +1,1 @@
 midlrt /metadata_dir %WINDIR%\System32\WinMetadata /h nul /nomidl /winrt /winmd metadata.winmd /reference %WINDIR%\System32\WinMetadata\Windows.Foundation.winmd metadata.idl
-mdmerge metadata.winmd
